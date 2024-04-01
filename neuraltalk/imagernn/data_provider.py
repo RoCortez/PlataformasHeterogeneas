8f6494727e09c1c1c8a35c20bd951e544eea72d0
RoCortez <94881893+RoCortez@users.noreply.github.com>
2024-04-01 16:30:44 -0700

    Update data_provider.py

--- neuraltalk/imagernn/data_provider.py
+++ neuraltalk/imagernn/data_provider.py
@@ -7,31 +7,31 @@ from collections import defaultdict
 
 class BasicDataProvider:
   def __init__(self, dataset):
-    print 'Initializing data provider for dataset %s...' % (dataset, )
+      print('Initializing data provider for dataset %s...' % (dataset, ))
 
-    # !assumptions on folder structure
-    self.dataset_root = os.path.join('data', dataset)
-    self.image_root = os.path.join('data', dataset, 'imgs')
+      # !assumptions on folder structure
+      self.dataset_root = os.path.join('data', dataset)
+      self.image_root = os.path.join('data', dataset, 'imgs')
 
-    # load the dataset into memory
-    dataset_path = os.path.join(self.dataset_root, 'dataset.json')
-    print 'BasicDataProvider: reading %s' % (dataset_path, )
-    self.dataset = json.load(open(dataset_path, 'r'))
+      # load the dataset into memory
+      dataset_path = os.path.join(self.dataset_root, 'dataset.json')
+      print('BasicDataProvider: reading %s' % (dataset_path, ))
+      self.dataset = json.load(open(dataset_path, 'r'))
 
-    # load the image features into memory
-    features_path = os.path.join(self.dataset_root, 'vgg_feats.mat')
-    print 'BasicDataProvider: reading %s' % (features_path, )
-    features_struct = scipy.io.loadmat(features_path)
-    self.features = features_struct['feats']
+      # load the image features into memory
+      features_path = os.path.join(self.dataset_root, 'vgg_feats.mat')
+      print('BasicDataProvider: reading %s' % (features_path, ))
+      features_struct = scipy.io.loadmat(features_path)
+      self.features = features_struct['feats']
 
-    # group images by their train/val/test split into a dictionary -> list structure
-    self.split = defaultdict(list)
-    for img in self.dataset['images']:
-      self.split[img['split']].append(img)
+      # group images by their train/val/test split into a dictionary -> list structure
+      self.split = defaultdict(list)
+      for img in self.dataset['images']:
+          self.split[img['split']].append(img)
 
   # "PRIVATE" FUNCTIONS
-  # in future we may want to create copies here so that we don't touch the 
-  # data provider class data, but for now lets do the simple thing and 
+  # in future we may want to create copies here so that we don't touch the
+  # data provider class data, but for now lets do the simple thing and
   # just return raw internal img sent structs. This also has the advantage
   # that the driver could store various useful caching stuff in these structs
   # and they will be returned in the future with the cache present
@@ -54,7 +54,7 @@ class BasicDataProvider:
 
   def getSplitSize(self, split, ofwhat = 'sentences'):
     """ return size of a split, either number of sentences or number of images """
-    if ofwhat == 'sentences': 
+    if ofwhat == 'sentences':
       return sum(len(img['sentences']) for img in self.split[split])
     else: # assume images
       return len(self.split[split])
@@ -96,7 +96,7 @@ class BasicDataProvider:
       yield batch
 
   def iterSentences(self, split = 'train'):
-    for img in self.split[split]: 
+    for img in self.split[split]:
       for sent in img['sentences']:
         yield self._getSentence(sent)
 
